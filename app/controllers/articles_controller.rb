2049c1285b53104741d28b3f162dbfa9d1ff5ba3
Caroline Dafonseca <dafonseca.ca@gmail.com>
2019-11-05 19:58:55 +0100

    create and update done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,27 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(params_create)
+    redirect_to article_path(@article.id)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.update(params_create)
+  end
+
+  private
+
+  def params_create
+    params.require(:article).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get 'articles/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/articles', to: 'articles#index'
+  patch '/articles/:id', to: 'articles#update'
+  post '/articles', to: 'articles#create'
+  get '/articles/new', to: 'articles#new'
   get '/articles/:id', to: 'articles#show', as: :article
+  get '/articles/:id/edit', to: 'articles#edit', as: :article_edit
 end
